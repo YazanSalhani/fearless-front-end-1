583247b834e36e01d48f671c0653649a97fae4d4
Arielle <arielleladams@gmail.com>
2024-06-25 17:43:54 -0400

    Added logic for columns, and added styling to cards

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,3 +1,16 @@
+function createCard(name, description, pictureUrl) {
+    return `
+    <div class="card" style="box-shadow: 5px 5px 5px grey; margin-bottom: 20px;">
+        <img src="${pictureUrl}" class="card-img-top">
+        <div class="card-body">
+            <h5 class="card-title">${name}</h5>
+            <p class="card-text">${description}</p>
+        </div>
+    </div>
+  `;
+}
+
+
 window.addEventListener('DOMContentLoaded', async () => {
     const url = 'http://localhost:8000/api/conferences/';
     try {
@@ -8,28 +21,35 @@ window.addEventListener('DOMContentLoaded', async () => {
         } else {
             const data = await response.json();
 
-            const conference = data.conferences[0];
-            const nameTag = document.querySelector('.card-title');
-            nameTag.innerHTML = conference.name;
+            let count = 1
+            for (let conference of data.conferences) {
+                const detailURL = `http://localhost:8000${conference.href}`;
+                const detailResponse = await fetch(detailURL);
 
-            const detailURL = `http://localhost:8000${conference.href}`;
-            const detailResponse = await fetch(detailURL);
-            
-            if (detailResponse.ok) {
-                const details = await detailResponse.json();
-                console.log(details)
+                if (detailResponse.ok) {
+                    const details = await detailResponse.json();
+                    const name = details.conference.name;
+                    const description = details.conference.description;
+                    const pictureUrl = details.conference.location.picture_url;
+                    const html = createCard(name, description, pictureUrl);
 
-                const description = details.conference.description
-                const descriptionTag = document.querySelector('.card-text');
-                descriptionTag.innerHTML = description;
-
-                const imageTag = document.querySelector('.card-img-top');
-                imageTag.src = details.conference.location.picture_url;
+                    let col
+                    if (count === 1) {
+                        col = '.col1';
+                        count++;
+                    } else if (count === 2) {
+                        col = '.col2';
+                        count++;
+                    } else if (count === 3) {
+                        col = '.col3';
+                        count = 1
+                    }
+                    const column = document.querySelector(col)
+                    column.innerHTML += html
+                }
             }
-
-        }
+            }
     } catch (error) {
         console.error('error', error);
     }
-
 });

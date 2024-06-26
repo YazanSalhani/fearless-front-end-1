e4f4878a68be334a1868b2fd52f0d447cb78dd82
Arielle <arielleladams@gmail.com>
2024-06-26 12:46:53 -0400

    Added create new location Form to HTML

--- ghi/js/new-location.js
+++ ghi/js/new-location.js
@@ -8,7 +8,7 @@ window.addEventListener("DOMContentLoaded", async () => {
 
         if (response.ok) {
             const data = await response.json();
-            console.log(data);
+            //console.log(data);
 
             const selectTag = document.getElementById('state');
             for (let state of data.states) {
@@ -22,7 +22,32 @@ window.addEventListener("DOMContentLoaded", async () => {
             throw new Error(`An error has occurred: ${response.status} (${response.statusText})`);
         };
 
+        const formTag = document.getElementById('create-location-form');
+        formTag.addEventListener('submit', async event => {
+            event.preventDefault();
+
+            const formData = new FormData(formTag);
+            const json =JSON.stringify(Object.fromEntries(formData));
+            //console.log(json);
+
+            const locationUrl = 'http://localhost:8000/api/locations/';
+            const fetchConfig = {
+                method: 'post',
+                body: json,
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            };
+
+            const locationResponse = await fetch(locationUrl, fetchConfig);
+            if (locationResponse.ok) {
+                formTag.reset();
+                const newLocation = await locationResponse.json();
+                //console.log(newLocation);
+            }
+        });
+
     } catch (error) {
         console.error('error', error);
     }
-})
+});
